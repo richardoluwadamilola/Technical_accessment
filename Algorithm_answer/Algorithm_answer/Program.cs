601d68126d8863730dcb13828acf1a2e42b73429
richardoluwadamilola <104902896+richardoluwadamilola@users.noreply.github.com>
2023-05-15 16:40:50 +0100

    accessment

--- Algorithm_answer/Algorithm_answer/Program.cs
+++ Algorithm_answer/Algorithm_answer/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main()
+    {
+        int[] input1 = { 1, 2, 3, 4, 5 };
+        int[] input2 = { 15, 25, 35 };
+        int[] input3 = { 8, 8 };
+
+        Console.WriteLine("Output: " + CalculateScore(input1));
+        Console.WriteLine("Output: " + CalculateScore(input2));
+        Console.WriteLine("Output: " + CalculateScore(input3));
+    }
+
+    public static int CalculateScore(int[] array)
+    {
+        int score = 0;
+
+        foreach (int item in array)
+        {
+            if (item % 2 == 0)
+                score += 1;
+            else
+                score += 3;
+
+            if (item == 8)
+                score += 5;
+        }
+
+        return score;
+    }
+}
